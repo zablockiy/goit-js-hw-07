ba4d14429f534911c39d6fd92d5b53c7a1f234b4
zablockiy <105744371+zablockiy@users.noreply.github.com>
2022-10-09 17:45:08 +0300

    01-gallery.js done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const imgGalleryEl = document.querySelector('.gallery');
+const markupGallery = createMakrUp(galleryItems);
+imgGalleryEl.innerHTML = markupGallery;
+imgGalleryEl.addEventListener('click', onClickImg);
+
+function createMakrUp(galleryItems) {
+    return galleryItems
+        .map((items) => {
+            return `<div class ="gallery__item">
+        <a class="gallery__link" href="${items.original}">
+        <img class="gallery__image" 
+        src="${items.preview}"
+        data-source="${items.original}"
+        alt="${items.description}"/>
+        </a>
+        </div>`
+        }).join("");
+}
+let instance = "";
+function onClickImg(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") { return };
+    instance = basicLightbox.create(
+    `<img src=${event.target.dataset.source}>`
+    );
+    instance.show();
+    document.addEventListener('keydown', onModalCloseToEscape); 
+    }
+    function onModalCloseToEscape (evt) {
+        if (evt.code === "Escape") {
+            instance.close();
+            document.removeEventListener("keydown", onModalCloseToEscape);
+
+        }
+}
